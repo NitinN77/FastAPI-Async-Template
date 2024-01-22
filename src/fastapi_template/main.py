940b01afd908a2eb2f519559dbed4176f3a8ccd0
NitinN77 <nitinnarayanan77@gmail.com>
2024-01-22 17:15:07 +0530

    add title and desc

--- src/fastapi_template/main.py
+++ src/fastapi_template/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(
+    title="FastAPI Template v1.0.0",
+    description="A pretty good template (I think)",
+    responses={404: {"message": "Page not found"}},
+)
 
 
 @app.get("/")
 def root():
-    return {"message": "hello from vercel"}
+    return {"message": "Root page :)"}
